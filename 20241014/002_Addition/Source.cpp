613c9d37a934a0428733acaf06dba590df9d6a33
Ramakant Debata <ramakant.s.debata@gmail.com>
2024-10-14 17:13:30 +0530

    Day 01

--- 20241014/002_Addition/Source.cpp
+++ 20241014/002_Addition/Source.cpp
@@ -17,9 +17,9 @@ int add(int a, int b)
 	int sum;
 	sum = a + b;
 
-	static int cumm_sum = 0;
-	cumm_sum += sum;
-	cout << cumm_sum << endl;
+	//static int cumm_sum = 0;
+	//cumm_sum += sum;
+	//cout << cumm_sum << endl;
 	return sum;
 }
 
@@ -33,29 +33,36 @@ int main()
 	result = add(p, q);
 	cout << "Sum --> " << result << endl;
 
-	add(1, 2);
-	add(4, 3);
-	add(50, 70);
+	
+	//// C- code
+	//int a = 10;
+	//printf("% d, % d", a++, a++);
 
-	int data;
-	extern int var;
-	cout << var << endl;
 
-	// some code
-	data = var;
 
-	// some code
-	data = var;
+	//add(1, 2);
+	//add(4, 3);
+	//add(50, 70);
 
-	// some code
-	data = var;
+	//int data;
+	//extern int var;
+	//cout << var << endl;
 
-	// some code
-	data = var;
+	//// some code
+	//data = var;
 
-	cout << data << endl;
-	//extern int inexistent_var;
-	//cout << inexistent_var << endl;
+	//// some code
+	//data = var;
+
+	//// some code
+	//data = var;
+
+	//// some code
+	//data = var;
+
+	//cout << data << endl;
+	////extern int inexistent_var;
+	////cout << inexistent_var << endl;
 
 	return 0;
 }
